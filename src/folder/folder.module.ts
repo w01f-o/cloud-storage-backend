45f5eeff215df4e73886e4e9fbd0a2b255976b66
w01f <arsenimarchenko@bk.ru>
2024-08-07 16:20:10 +0300

    fix: Added JwtService in folder module

--- src/folder/folder.module.ts
+++ src/folder/folder.module.ts
@@ -3,9 +3,10 @@ import { FolderService } from './folder.service';
 import { FolderController } from './folder.controller';
 import { DatabaseService } from 'src/database/database.service';
 import { TokenService } from 'src/token/token.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   controllers: [FolderController],
-  providers: [FolderService, DatabaseService, TokenService],
+  providers: [FolderService, DatabaseService, TokenService, JwtService],
 })
 export class FolderModule {}
