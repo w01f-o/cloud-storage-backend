0f6c058e07253df98237652e94daadf182328822
w01f <arsenimarchenko@bk.ru>
2024-08-08 18:39:54 +0300

    ref: Remove empty functions of user service

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
-import { MailController } from './mail.controller';
 
 @Module({
-  controllers: [MailController],
   providers: [MailService],
 })
 export class MailModule {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,24 +1,4 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UserService {
-  create() {
-    return 'This action adds a new user';
-  }
-
-  findAll() {
-    return `This action returns all user`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update() {
-    return `This action updates a user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
-}
+export class UserService {}
